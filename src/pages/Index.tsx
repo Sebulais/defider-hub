0bd7d76a10e67be8bc0e3bfacd315ac20293113f
Noch-Voron <116518720+Noch-Voron@users.noreply.github.com>
2025-09-30 16:30:55 -0300

    Update landing page 3/3

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,13 +1,11 @@
 import Hero from "@/components/Hero";
 import ServicesOverview from "@/components/ServicesOverview";
-import WorkshopPreview from "@/components/WorkshopPreview";
 
 const Index = () => {
   return (
     <div className="min-h-screen">
       <Hero />
       <ServicesOverview />
-      <WorkshopPreview />
     </div>
   );
 };
