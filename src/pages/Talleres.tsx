70450ac4ca469abebd8ab0f688bb5b82cb6f9eb1
Noch-Voron <116518720+Noch-Voron@users.noreply.github.com>
2025-09-29 22:09:15 -0300

    Deleted Clock icon

--- src/pages/Talleres.tsx
+++ src/pages/Talleres.tsx
@@ -7,7 +7,6 @@ import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { 
   Users, 
-  Clock, 
   MapPin, 
   Star,
   Calendar,
